b250da616451fdb7eb31a9e96c7fead6d4e1e54b
Gavin Noronha <gavinln@hotmail.com>
2014-10-12 20:29:55 -0700

    Added fig for Python

--- puppet/manifests/classes/python_setup.pp
+++ puppet/manifests/classes/python_setup.pp
@@ -18,6 +18,11 @@ class python_setup {
             #    provider => pip,
             #    require => Package['python-pip']
             #}
+            package { 'fig':
+                ensure => installed,
+                provider => pip,
+                require => Package['python-pip']
+            }
             package { 'tornado':
                 ensure => installed,
                 provider => pip,
