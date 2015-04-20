52c73a0f9e46fb1544e493af470a7d67a81dd8e3
Gavin Noronha <gavinln@hotmail.com>
2015-04-19 18:50:52 -0700

    added

--- scripts/slave_mesos_start.sh
+++ scripts/slave_mesos_start.sh
@@ -3,8 +3,9 @@
 #sudo mesos-slave --master=192.168.33.10:5050
 #sudo mesos-slave --master=zk://192.168.33.10:2181/mesos --log_dir=/var/log/mesos
 #sudo mesos-slave --master=zk://localhost:2181/mesos --log_dir=/var/log/mesos
-
 #sudo mesos-slave --master=zk://localhost:2181/mesos --log_dir=/var/log/mesos --isolation=cgroups/cpu,cgroups/mem
 
-sudo mesos-slave --containerizers=docker,mesos --master=zk://localhost:2181/mesos --log_dir=/var/log/mesos --executor_registration_timeout=5mins --resources="ports(*):[31000-41099]"
+sudo mesos-slave --containerizers=docker,mesos --master=zk://localhost:2181/mesos --log_dir=/var/log/mesos \
+	--executor_registration_timeout=5mins --resources="ports(*):[31000-41099]"	\
+	--credential=cred_slave.json
 
