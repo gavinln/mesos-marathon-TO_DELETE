2244699db1a2f1f93a54d03352118c3f5eea055a
Gavin Noronha <gnoronha@financialengines.com>
2015-04-22 13:19:27 -0700

    added code to automatically set IP

--- scripts/master_mesos_start.sh
+++ scripts/master_mesos_start.sh
@@ -2,4 +2,6 @@
 
 #sudo mesos-master --ip=192.168.33.10 --work_dir=/var/lib/mesos
 #sudo mesos-master --ip=192.168.0.149 --work_dir=/var/lib/mesos --zk=zk://192.168.0.149:2181/mesos --quorum=1
-sudo mesos-master --ip=192.168.33.10 --work_dir=/var/lib/mesos --zk=zk://localhost:2181/mesos --log_dir=/var/log/mesos --quorum=1 --credentials=cred_master.json
+
+IP=$(ip -o -4 addr show eth1 | grep -o 'inet [0-9\.]\+' | cut -f2 -d' ')
+sudo mesos-master --ip=$IP --work_dir=/var/lib/mesos --zk=zk://localhost:2181/mesos --log_dir=/var/log/mesos --quorum=1 --credentials=cred_master.json
