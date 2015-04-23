34594c99068889c0bb37cb16dc4be443e6ea7d7e
Gavin Noronha <gavinln@hotmail.com>
2015-04-22 17:57:32 -0700

    ignore log files and added examples of executing commands

--- scripts/execute_mesos.sh
+++ scripts/execute_mesos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#mesos execute --command="ls -lrt"  --master=192.168.33.10:5050 --name=ls
-mesos execute --command="pwd"  --master=192.168.33.10:5050 --name=pwd
+#sudo mesos execute --master=192.168.0.106:5050 --command="ls -lrt" --name=ls
+sudo mesos execute --master=192.168.0.106:5050 --docker_image=busybox:latest --command="ls -lrt" --name=docker_ls
