68eeea4930d690938595de24884248d998387fa2
Gavin Noronha <gavinln@hotmail.com>
2015-05-02 14:55:29 -0700

    dynamically getting ip

--- scripts/execute_mesos.sh
+++ scripts/execute_mesos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#sudo mesos execute --master=192.168.0.106:5050 --command="ls -lrt" --name=ls
-sudo mesos execute --master=192.168.0.106:5050 --docker_image=busybox:latest --command="ls -lrt" --name=docker_ls
+IP=$(ip -o -4 addr show eth1 | grep -o 'inet [0-9\.]\+' | cut -f2 -d' ')
+
+sudo mesos execute --master=$IP:5050 --command="ls -lrt" --name=ls
+#sudo mesos execute --master=192.168.0.106:5050 --docker_image=busybox:latest --command="ls -lrt" --name=docker_ls
